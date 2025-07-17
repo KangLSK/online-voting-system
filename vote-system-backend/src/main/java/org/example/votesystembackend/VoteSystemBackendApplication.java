055b571f0da7f0ebe08188ab817b1751c6bd3a7b
KangLSK <36182682+0961191926@users.noreply.github.com>
2025-07-17 17:45:22 +0800

    setup full project with backend and frontend structure

---
+++ vote-system-backend/src/main/java/org/example/votesystembackend/VoteSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.votesystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteSystemBackendApplication.class, args);
+    }
+
+}
---
+++ vote-system-backend/src/test/java/org/example/votesystembackend/VoteSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.votesystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
